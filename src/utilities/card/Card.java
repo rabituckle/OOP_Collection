401b77940875fa345168ebad899fb308bc5d99e9
LeeVu-S-1345 <levuchuyentoanls@gmail.com>
2025-05-07 16:10:27 +0700

    Delete src directory

--- src/utilities/card/Card.java
+++
@@ -1,9 +0,0 @@
-package utilities.card;
-
-public abstract class Card {
-	
-	public abstract String getValue();
-	public abstract int getCost();
-	public abstract int compare(Card card);
-	public abstract String getRank();
-}
